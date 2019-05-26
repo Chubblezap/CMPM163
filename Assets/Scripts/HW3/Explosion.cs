9c39c77fd4348164fdda43fd09b1b5fdefd00be8
Chubblezap <ajwiley@ucsc.edu>
2019-05-25 23:07:26 -0700

    HW3

---
+++ Assets/Scripts/HW3/Explosion.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    Renderer render;
+    ParticleSystem ps;
+    float timer;
+    float curExAmnt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        curExAmnt = 1;
+        render = GetComponent<ParticleSystemRenderer>();
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= 1)
+            timer = 0;
+        render.material.SetFloat("_DissolveAmount", timer);
+
+        var sz = ps.sizeOverLifetime;
+
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            curExAmnt += 0.1f;
+            render.material.SetFloat("_ExplodeAmount", curExAmnt);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            curExAmnt -= 0.1f;
+            render.material.SetFloat("_ExplodeAmount", curExAmnt);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            sz.sizeMultiplier += 10;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            sz.sizeMultiplier -= 10;
+        }
+    }
+}
---
+++ Assets/Scripts/HW3/SoundFire.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundFire : MonoBehaviour
+{
+    ParticleSystem ps;
+
+    public AudioSource audioSource;
+    private float updateStep = 0.01f;
+    private int sampleDataLength = 1024;
+
+    private float currentUpdateTime = 0f;
+
+    private float clipLoudness;
+    private float[] clipSampleData;
+
+    private void Awake()
+    {
+        if (!audioSource)
+        {
+            Debug.LogError(GetType() + ".Awake: there was no audioSource set.");
+        }
+        clipSampleData = new float[sampleDataLength];
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentUpdateTime += Time.deltaTime;
+        if (currentUpdateTime >= updateStep)
+        {
+            currentUpdateTime = 0f;
+            audioSource.clip.GetData(clipSampleData, audioSource.timeSamples); //I read 1024 samples, which is about 80 ms on a 44khz stereo clip, beginning at the current sample position of the clip.
+            clipLoudness = 0f;
+            foreach (var sample in clipSampleData)
+            {
+                clipLoudness += Mathf.Abs(sample);
+            }
+            clipLoudness /= sampleDataLength; //clipLoudness is what you are looking for
+            var sz = ps.sizeOverLifetime;
+            sz.sizeMultiplier = clipLoudness * 5;
+        }
+    }
+}
